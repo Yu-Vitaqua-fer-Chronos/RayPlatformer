343c7c3a39e6635d78b448d26cd336b1dbe2bd6e
Technisha <proxneon960@gmail.com>
2022-08-15 19:48:54 +0100

    Everything

--- setup-env.sh
+++ setup-env.sh
@@ -2,8 +2,8 @@
 
 nimble install https://github.com/enthus1ast/nim-chipmunk nake --accept
 
-git clone https://github.com/slembcke/Chipmunk2D chipmunk --depth 1
-git clone https://github.com/planetis-m/naylibhttps://github.com/Planetis-M/Naylib naylib --depth 1
+git clone https://github.com/Slembcke/Chipmunk2D chipmunk --depth 1
+git clone https://github.com/Planetis-M/Naylib naylib --depth 1
 
 cd naylib
 nake buildDesktop
